948e31af93313ae9581ae9de06d02af2b481a433
Kerri Shotts <kerrishotts@gmail.com>
2015-11-26 00:18:32 -0600

    trying hugo

---
+++ tags/q/index.js
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+  	<meta property="og:title" content=" Q.Js &middot;  Kerri Shotts" />
+  	<meta property="og:site_name" content="Kerri Shotts" />
+  	<meta property="og:url" content="http://kerrishotts.github.io/tags/q/index.js/" />
+
+    
+    <meta property="og:type" content="website" />
+    
+
+  <title>
+     Q.Js &middot;  Kerri Shotts
+  </title>
+
+    <meta name="description" content="Thoughts from beyond..." />
+
+    <meta name="HandheldFriendly" content="True" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="shortcut icon" href="http://kerrishotts.github.io/images/favicon.ico">
+	  <link rel="apple-touch-icon" href="http://kerrishotts.github.io/images/apple-touch-icon.png" />
+
+    <link rel="stylesheet" type="text/css" href="http://kerrishotts.github.io/css/screen.css" />
+    <link rel="stylesheet" type="text/css" href="http://kerrishotts.github.io/css/nav.css" />
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Merriweather:300,700,700italic,300italic|Open+Sans:700,400|Inconsolata" />
+
+
+    
+      
+          <link href="http://kerrishotts.github.io/index.xml" rel="alternate" type="application/rss+xml" title="Kerri Shotts" />
+      
+      
+        <link href="http://kerrishotts.github.io/tags/q.js/index.xml" rel="alternate" type="application/rss+xml" title="Q.Js &middot; Kerri Shotts" />
+      
+    
+    <meta name="generator" content="Hugo 0.14" />
+
+    <link rel="canonical" href="http://kerrishotts.github.io/tags/q/index.js/" />
+
+    
+
+    
+</head>
+<body class="nav-closed">
+
+  <div class="nav">
+    <h3 class="nav-title">Menu</h3>
+    <a href="#" class="nav-close">
+        <span class="hidden">Close</span>
+    </a>
+    <ul>
+        
+        
+        
+            
+            <li class="nav-opened" role="presentation">
+            	<a href="http://kerrishotts.github.io/">Home</a>
+            </li>
+        
+            
+            <li class="nav-opened" role="presentation">
+            	<a href="http://kerrishotts.github.io/page/about/">About Kerri</a>
+            </li>
+        
+            
+            <li class="nav-opened" role="presentation">
+            	<a href="http://kerrishotts.github.io/page/contact/">Contact Kerri</a>
+            </li>
+        
+    </ul>
+    
+    
+    <a class="subscribe-button icon-feed" href="http://kerrishotts.github.io/tags/q.js/index.xml">Subscribe</a>
+    
+</div>
+<span class="nav-cover"></span>
+
+
+ <div class="site-wrapper">
+
+
+
+
+	<header class="main-header tag-head no-cover">
+
+    <nav class="main-nav overlay clearfix">
+      
+        <a class="blog-logo" href="http://kerrishotts.github.io/"><img src="http://kerrishotts.github.io/images/avatar.jpg" alt="Home" /></a>
+      
+      
+          <a class="menu-button" href="#"><span class="burger">&#9776;</span><span class="word">Menu</span></a>
+      
+    </nav>
+    <div class="vertical">
+        <div class="main-header-content inner">
+            <h1 class="page-title">Q.Js</h1>
+            <h2 class="page-description">
+                
+            </h2>
+        </div>
+    </div>
+</header>
+
+<main class="content" role="main">
+    
+
+	<div class="extra-pagination inner">
+    <nav class="pagination" role="navigation">
+	
+	<span class="page-number">Page 1 of 1</span>
+	
+</nav>
+
+	</div>
+
+	
+	   
+<article class="post post">
+    <header class="post-header">
+        <h2 class="post-title"><a href="http://kerrishotts.github.io/post/2013/10/26">Using the File API without callbacks</a></h2>
+    </header>
+    <section class="post-excerpt">
+        <p><p>So, it&rsquo;s pretty much a given that working with the HTML5 API is a painful experience due to all those levels of callbacks. Even
+a simple operation turns into something unnecessarily complex, and so I present FileManager &ndash; a simple wrapper around frequently
+used portions of the File API that uses Promises (or Q.js, specifically).</p>
+ <a class="read-more" href="http://kerrishotts.github.io/post/2013/10/26">&raquo;</a></p>
+    </section>
+    <footer class="post-meta">
+        
+        
+            <img class="author-thumb" src="http://kerrishotts.github.io/images/avatar.jpg" alt="Author image" nopin="nopin" />
+        
+        
+            Kerri Shotts
+        
+        on
+            
+                <a href="http://kerrishotts.github.io/tags/cordova/">#cordova</a>,
+            
+                <a href="http://kerrishotts.github.io/tags/promises/">#promises</a>,
+            
+                <a href="http://kerrishotts.github.io/tags/file-api/">#file api</a>,
+            
+                <a href="http://kerrishotts.github.io/tags/phonegap/">#phonegap</a>,
+            
+                <a href="http://kerrishotts.github.io/tags/q.js/">#q.js</a>,
+            
+                <a href="http://kerrishotts.github.io/tags/yasmf/">#yasmf</a>,
+            
+                <a href="http://kerrishotts.github.io/tags/filemanager/">#filemanager</a>,
+            
+        
+        <time class="post-date" datetime="2013-10-26T00:00:00Z">
+            26 Oct 2013
+        </time>
+    </footer>
+</article>
+
+	
+
+	<nav class="pagination" role="navigation">
+	
+	<span class="page-number">Page 1 of 1</span>
+	
+</nav>
+
+</main>
+
+
+
+    <footer class="site-footer clearfix">
+        <section class="copyright"><a href="">Kerri Shotts</a> Copyright © 2015 Kerri Shotts. All Rights Reserved.</section>
+        
+        <section class="poweredby">Proudly generated by <a class="icon-hugo" href="http://gohugo.io">HUGO</a>, with <a class="icon-theme" href="https://github.com/vjeantet/hugo-theme-casper">Casper</a> theme</section>
+        
+    </footer>
+    </div>
+    <script type="text/javascript" src="http://kerrishotts.github.io/js/jquery.js"></script>
+    <script type="text/javascript" src="http://kerrishotts.github.io/js/jquery.fitvids.js"></script>
+    <script type="text/javascript" src="http://kerrishotts.github.io/js/index.js"></script>
+    
+</body>
+</html>
